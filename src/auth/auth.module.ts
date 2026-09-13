6c239e31998f124f04629b1413a7f6c939cf9035
Elia Giolli <eliagiolli22@gmail.com>
2026-09-13 14:29:18 +0200

    installed the packages for @nest/jwt and configured basic strategy with jwt.

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+
+import { jwtConstants } from './constants.js'
+import { UsersModule } from '../users/users.module.js';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }
+    })
+  ]
+})
+export class AuthModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
+import { User } from './entities/user.entity.js';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +13,8 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(username:string):Promise<User | undefined> {
+    return this.findOne(username);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
