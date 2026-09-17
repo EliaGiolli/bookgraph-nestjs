3eb16ff3cbb62390b11da27a82be97faf1415700
Elia Giolli <eliagiolli22@gmail.com>
2026-09-17 10:48:06 +0200

    created custom book-connections entity and book-connections dto

--- src/book-connections/dto/create-book-connection.dto.ts
+++ src/book-connections/dto/create-book-connection.dto.ts
@@ -1,1 +1,19 @@
-export class CreateBookConnectionDto {}
+import { IsUUID, IsString, IsOptional, ValidateIf } from 'class-validator';
+
+export class CreateBookConnectionDto {
+  @IsUUID()
+  sourceBookId: string;
+
+  @IsUUID()
+  @ValidateIf((o: CreateBookConnectionDto) => {
+    if (o.discoveredBookId === o.sourceBookId) {
+      throw new Error('discoveredBookId must be different from sourceBookId');
+    }
+    return true;
+  })
+  discoveredBookId: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
--- src/book-connections/entities/book-connection.entity.ts
+++ src/book-connections/entities/book-connection.entity.ts
@@ -1,1 +1,28 @@
-export class BookConnection {}
+import * as typeorm from 'typeorm';
+import { Book } from '../../books/entities/books.entity.js';
+
+@typeorm.Entity('book_connections')
+export class BookConnection {
+  @typeorm.PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @typeorm.Column({ type: 'uuid' })
+  userId: string;
+
+  @typeorm.Column({ type: 'uuid' })
+  sourceBookId: string;
+
+  @typeorm.ManyToOne(() => Book, { onDelete: 'CASCADE' })
+  @typeorm.JoinColumn({ name: 'sourceBookId' })
+  sourceBook: typeorm.Relation<Book>;
+
+  @typeorm.Column({ type: 'uuid' })
+  discoveredBookId: string;
+
+  @typeorm.ManyToOne(() => Book, { onDelete: 'CASCADE' })
+  @typeorm.JoinColumn({ name: 'discoveredBookId' })
+  discoveredBook: typeorm.Relation<Book>;
+
+  @typeorm.Column({ type: 'varchar', length: 100, nullable: true })
+  description?: string;
+}
